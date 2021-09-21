a8cd04276a9bc1a3419227bbe5a370d0a0ff57e8
Max Bay <cwang167@my.centennialcollege.ca>
2021-09-21 14:07:36 +0800

    Update 2021.09.21: Chasing functionality completed

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -21,6 +21,11 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private GameObject playerGO;
 
+    // patrolling path points
+    public Transform[] wayPoints;
+    public int currentWayPointsIndex;
+    public float enemySpeed = 1.0f; // 1m/s
+
     // Start is called before the first frame update
     void Start()
     {
@@ -59,15 +64,19 @@ public class EnemyController : MonoBehaviour
         {
             ChangeState(EnemyState.Chasing);
         }
+
+        FollowPatrollingPath();
     }
 
     public void HandleChasingState()
     {
-        Debug.Log(currentState);
+        
         if (!CanSeePlayer())
         {
             ChangeState(EnemyState.Patrolling);
         }
+
+        this.transform.position = MyMoveTowards(this.transform.position, playerGO.transform.position, enemySpeed*Time.deltaTime*1.5f);
     }
 
     public void HandleAttachkingState()
@@ -92,5 +101,58 @@ public class EnemyController : MonoBehaviour
         canSeePlayer = distance<= distanceToChase && cosAngle_enemy2player>0;
 
         return canSeePlayer;
+    }
+
+    private void FollowPatrollingPath()
+    {
+        Vector3 target = wayPoints[currentWayPointsIndex].position;
+        //Vector3.Distance(this.transform.position, target) < 0.1f
+        //if (this.transform.position==target)
+        if(Vector3.Distance(this.transform.position, target) < 0.1f)
+        {
+            currentWayPointsIndex = CalculateNextWayPointIndex();
+            target = wayPoints[currentWayPointsIndex].transform.position;
+        }
+
+        //this.transform.position = Vector3.MoveTowards(this.transform.position, target,enemySpeed*Time.deltaTime);
+        this.transform.position = MyMoveTowards(this.transform.position, target, enemySpeed * Time.deltaTime);
+
+    }
+
+    private Vector3 MyMoveTowards(Vector3 current, Vector3 target, float maxDistanceDelta)
+    {
+        Vector3 current2Target = target - current;
+
+        //-------- Start of Changes related to Rotation -------------
+        //this.transform.LookAt(target); //Abrupt1: this is a too abrupt rotation; see also Abrupt2 below
+
+        Quaternion qtargetrotation = Quaternion.LookRotation(current2Target);
+        //this.transform.rotation = qtargetrotation;  //Abrupt2: this is a too abrupt rotation; not very beleivable; to try, uncomment this and comment out the line below
+
+        // This is the smoothest rotation; more beleivable
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, qtargetrotation, Time.deltaTime);
+        //-------- End of Changes related to Rotation -------------
+
+        Vector3 movement = current + current2Target.normalized * maxDistanceDelta;
+        return movement;
+
+    }
+
+
+
+    private int CalculateNextWayPointIndex()
+    {
+        return ((currentWayPointsIndex + 1) % wayPoints.Length);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        
+
+        for (int i = 0; i < wayPoints.Length; i++)
+        {
+            Gizmos.DrawLine(wayPoints[i].position, wayPoints[(i + 1) % wayPoints.Length].position);
+        }
     }
 }
